c8421d852c17c86583fcb0c0e90bba67d76d6038
LuckyLMJ <70863143+LuckyLMJ@users.noreply.github.com>
2023-10-17 08:33:50 -0400

    fixed stupid formatting issues

--- technology.lua
+++ technology.lua
@@ -40,86 +40,86 @@ data:extend(
             order = "d-a"
         },
         {
-          type = "technology",
-          name = "automation-science-pack",
-          icon_size = 64,
-          icon_mipmaps = 4,
-          icons = {
-              {
-                  icon = "__base__/graphics/icons/automation-science-pack.png",
-                  tint = {1, 1, 1}
-              }
-          },
-          effects =
-          {
-              {
-                  type = "unlock-recipe",
-                  recipe = "automation-science-pack"
-              }
-          },
-          unit =
-          {
-              count = 50,
-              ingredients = {{"burner-science-pack", 1}},
-              time = 5
-          },
-          prerequisites = {"glass-processing"},
-          order = "d-a"
+          	type = "technology",
+          	name = "automation-science-pack",
+          	icon_size = 64,
+          	icon_mipmaps = 4,
+          	icons = {
+            	{
+                	icon = "__base__/graphics/icons/automation-science-pack.png",
+                  	tint = {1, 1, 1}
+              	}
+          	},
+          	effects =
+          	{
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "automation-science-pack"
+              	}
+          	},
+          	unit =
+          	{
+              	count = 50,
+              	ingredients = {{"burner-science-pack", 1}},
+              	time = 5
+          	},
+          	prerequisites = {"glass-processing"},
+          	order = "d-a"
         },
         {
-          type = "technology",
-          name = "electricity",
-          icon_size = 64,
-          icon_mipmaps = 4,
-          icons = {
-              {
-                  icon = "__base__/graphics/icons/small-electric-pole.png",
-                  tint = {1, 1, 1}
-              }
-          },
-          effects =
-          {
-              {
-                  type = "unlock-recipe",
-                  recipe = "small-electric-pole"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "steam-engine"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "inserter"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "lab"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "electric-mining-drill"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "boiler"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "offshore-pump"
-              },
-              {
-                  type = "unlock-recipe",
-                  recipe = "alt-electronic-circuit-wood"
-              }
-          },
-          unit =
-          {
-              count = 50,
-              ingredients = {{"burner-science-pack", 1}, {"automation-science-pack", 1}},
-              time = 10
-          },
-          prerequisites = {"automation-science-pack"},
-          order = "d-a"
+          	type = "technology",
+          	name = "electricity",
+          	icon_size = 64,
+          	icon_mipmaps = 4,
+          	icons = {
+              	{
+                  	icon = "__base__/graphics/icons/small-electric-pole.png",
+                  	tint = {1, 1, 1}
+              	}
+          	},
+          	effects =
+          	{
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "small-electric-pole"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "steam-engine"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "inserter"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "lab"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "electric-mining-drill"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "boiler"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "offshore-pump"
+              	},
+              	{
+                  	type = "unlock-recipe",
+                  	recipe = "alt-electronic-circuit-wood"
+              	}
+          	},
+          	unit =
+         	{
+              	count = 50,
+              	ingredients = {{"burner-science-pack", 1}, {"automation-science-pack", 1}},
+              	time = 10
+          	},
+          	prerequisites = {"automation-science-pack"},
+          	order = "d-a"
         }
     }
 )
